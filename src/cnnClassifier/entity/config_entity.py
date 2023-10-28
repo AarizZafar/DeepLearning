97197db51cb54f631e328919511d29b04cd8cef3
AarizZafar <aariz.zafar01@gmail.com>
2023-10-28 15:23:06 +0530

    callbacks added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 @dataclass(frozen = True)
 class DataIngestionConfig:  #(BELOW ARE THE RETURN TYPES OF THE FUNCTION)
-    root_dir         : Path
-    source_URL       : str
-    local_data_file  : Path
-    unzip_dir        : Path
+    root_dir                  : Path
+    source_URL                : str
+    local_data_file           : Path
+    unzip_dir                 : Path
 
 @dataclass(frozen = True)
 class PrepareBaseModelConfig:
@@ -18,3 +18,9 @@ class PrepareBaseModelConfig:
     params_include_top        : bool
     params_weights            : str
     params_classes            : int
+
+@dataclass(frozen = True)
+class PrepareCallbacksConfig:
+    root_dir                  : Path
+    tensorboard_root_log_dir  : Path
+    checkpoint_model_filepath : Path
